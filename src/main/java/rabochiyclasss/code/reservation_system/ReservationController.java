4f24cb90978a82c5d5c1c85125e384f01bb2331c
rabochiyclasss <20budkoivan02@gmail.com>
2026-01-03 15:39:36 +0100

    Controller and Service Added

---
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
@@ -0,0 +1,20 @@
+package rabochiyclasss.code.reservation_system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping()
+    public String getReservationById() {
+        return reservationService.getReservationById();
+    }
+
+}
---
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationService.java
@@ -0,0 +1,12 @@
+package rabochiyclasss.code.reservation_system;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+
+    public String getReservationById() {
+        return "reservation-1";
+    }
+
+}
