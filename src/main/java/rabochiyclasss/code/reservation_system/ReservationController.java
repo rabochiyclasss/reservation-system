2c8ed11eaa660e21608e1492a46fbe8cdf0e1212
rabochiyclasss <20budkoivan02@gmail.com>
2026-01-03 16:39:03 +0100

    data is hard coded onto Reservation class to check Controller performance

--- src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
@@ -1,6 +1,7 @@
 package rabochiyclasss.code.reservation_system;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,9 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
-    @GetMapping()
-    public String getReservationById() {
-        return reservationService.getReservationById();
+    @GetMapping("/{id}")
+    public Reservation getReservationById(@PathVariable("id") Long id) {
+        return reservationService.getReservationById(id);
     }
 
 }
--- src/main/java/rabochiyclasss/code/reservation_system/ReservationService.java
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationService.java
@@ -2,11 +2,14 @@ package rabochiyclasss.code.reservation_system;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 public class ReservationService {
 
-    public String getReservationById() {
-        return "reservation-1";
+    public Reservation getReservationById(Long id) {
+        return new Reservation(id, 100L, 40L, LocalDate.now(),
+                LocalDate.now().plusDays(5), ReservationStatus.APPROVED);
     }
 
 }
