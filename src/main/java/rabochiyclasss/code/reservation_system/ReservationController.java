366f0fb6a677de410eaac9fdbb2c9b17016c9f82
rabochiyclasss <20budkoivan02@gmail.com>
2026-01-03 17:28:02 +0100

    Service logic refactored

--- src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ReservationController {
 
@@ -16,6 +18,11 @@ public class ReservationController {
     @GetMapping("/{id}")
     public Reservation getReservationById(@PathVariable("id") Long id) {
         return reservationService.getReservationById(id);
+    }
+
+    @GetMapping
+    public List<Reservation> getAllReservations() {
+        return reservationService.findAllReservation();
     }
 
 }
--- src/main/java/rabochiyclasss/code/reservation_system/ReservationService.java
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationService.java
@@ -3,13 +3,32 @@ package rabochiyclasss.code.reservation_system;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReservationService {
 
+    private final Map<Long, Reservation> reservationMap = Map.of(
+        1L, new Reservation(1L, 100L, 40L, LocalDate.now(),
+                    LocalDate.now().plusDays(5), ReservationStatus.APPROVED),
+
+            2L, new Reservation(2L, 101L, 41L, LocalDate.now(),
+                    LocalDate.now().plusDays(6), ReservationStatus.APPROVED),
+            3L, new Reservation(3L, 102L, 42L, LocalDate.now(),
+                    LocalDate.now().plusDays(7), ReservationStatus.APPROVED)
+    );
+
     public Reservation getReservationById(Long id) {
-        return new Reservation(id, 100L, 40L, LocalDate.now(),
-                LocalDate.now().plusDays(5), ReservationStatus.APPROVED);
+        if (!reservationMap.containsKey(id)) {
+            throw new NoSuchElementException("Not found reservation by id = " + id);
+        }
+        return reservationMap.get(id);
     }
 
+    public List<Reservation> findAllReservation() {
+        return reservationMap.values().stream().toList();
+    }
 }
