ee241202a052cb63cbb2a0ecaccf4637733de1de
rabochiyclasss <20budkoivan02@gmail.com>
2026-01-03 17:44:48 +0100

    logs added to controllers

--- src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
+++ src/main/java/rabochiyclasss/code/reservation_system/ReservationController.java
@@ -1,13 +1,18 @@
 package rabochiyclasss.code.reservation_system;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class ReservationController {
+
+    private static final Logger log = LoggerFactory.getLogger(ReservationController.class);
 
     private final ReservationService reservationService;
 
@@ -17,11 +22,13 @@ public class ReservationController {
 
     @GetMapping("/{id}")
     public Reservation getReservationById(@PathVariable("id") Long id) {
+        log.info("Called getReservationById: id = {}", id);
         return reservationService.getReservationById(id);
     }
 
     @GetMapping
     public List<Reservation> getAllReservations() {
+        log.info("Called getAllReservations");
         return reservationService.findAllReservation();
     }
 
